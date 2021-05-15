e88f34d5f5e9277394e9f1b535c53b16ca9e2d87
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-05-15 23:54:23 +0900

    add Weather function on map.js

--- map-api/app.js
+++ map-api/app.js
@@ -3,24 +3,32 @@ const express = require('express');
 const path = require('path');
 const request = require('request');
 
+// CORS 때문에 추가한 부분 //
+const cors = require('cors');
+
+
 // express 는 함수이므로, 반환값을 변수에 저장한다.
 const app = express();
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(cors({origin: 'http://localhost:8080'}));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 3000 포트로 서버 오픈
-app.listen(8080, function(req,res) {
-    console.log("start! express server on port 8080");
+app.listen(8080, function (req, res) {
+  console.log("start! express server on port 8080");
 });
 
-app.get('/', function(req,res){
-  res.sendfile(__dirname+"/public/index.html");
+app.get('/',  function (req, res) {
+  res.sendfile(__dirname + "/public/index.html");
+  res.header("Access-Control-Allow-Origin", "*");
+  
 });
 
-app.get('/index', function(req,res){
-  res.sendfile(__dirname+"/public/index.html");
-});
+// app.get('/index', function(req,res){
+//   res.sendfile(__dirname+"/public/index.html");
+// });
 
-app.get('/geolocation', function(req,res){
-  res.sendfile(__dirname+"/public/geolocation.html");
-});
+// app.get('/geolocation', function(req,res){
+//   res.sendfile(__dirname+"/public/geolocation.html");
+// });
