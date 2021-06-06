69d77e71e8d9c464b31a2202feddd7f2fde3229b
kangho-Noh <rkdgh98@khu.ac.kr>
2021-06-06 18:14:38 +0900

    Add additional Info display on sidebar when a marker clicked.

--- src/components/FoodButtons.js
+++ src/components/FoodButtons.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "./FoodButtons.css";
 import "semantic-ui-css/semantic.min.css";
+import "./FoodButtons.css";
 //img의 alt와 title은 사진에 마우스 올렸을 때 나오는 제목 표시하기 위함
 //css를 하기 위해서 style={{}}를 사용할 수 있다.
 class FoodButtons extends React.Component {
@@ -11,7 +11,7 @@ class FoodButtons extends React.Component {
   setButtonColor() {
     const { buttonIndex } = this.props;
     if (buttonIndex.length) {
-      console.log(buttonIndex);
+      //console.log(buttonIndex);
       for (let i = 0; i < buttonIndex.length; i++) {
         let btn = document.getElementById(buttonIndex[i]);
         btn.classList.add(`button_${i}`);
